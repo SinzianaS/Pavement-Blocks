9f78a98be5d8b7f6c7d555b30c37de308b5fcf3f
Sinziana <sinzi@Sinzianas-MacBook-Air.local>
2022-04-29 20:42:01 +0300

    app to verify number of blocks needed to pave market square

---
+++ pavementBlocks/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace pavementBlocks
+{
+
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+            decimal marketSquareM = Convert.ToDecimal(Console.ReadLine());
+            decimal marketSquareN = Convert.ToDecimal(Console.ReadLine());
+            decimal marketBlocksA = Convert.ToDecimal(Console.ReadLine());
+            decimal ariaDreptunghiului = Math.Ceiling(marketSquareM / marketBlocksA) * Math.Ceiling(marketSquareN / marketBlocksA);
+            decimal v = Math.Ceiling(ariaDreptunghiului);
+            Console.WriteLine(v);
+            Console.Read();
+
+
+        }
+
+    }
+}
+    
+
